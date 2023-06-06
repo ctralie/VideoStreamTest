a62c3832c88f9f47198196e0b4dff1e6acfd993d
Chris Tralie <chris.tralie@gmail.com>
2023-06-06 16:55:01 -0400

    Making it a bit bigger

--- canvas.js
+++ canvas.js
@@ -6,7 +6,7 @@ class CameraCanvas {
         let video = document.createElement("video");
         //video.style = "display:none;";
         // Suggested on https://github.com/jeeliz/jeelizFaceFilter/issues/14#issuecomment-682209245
-        video['style']['transform'] = 'scale(0.0001,0.0001)';
+        video['style']['transform'] = 'scale(0.1,0.1)';
         video['style']['position'] = 'fixed';
         video['style']['bottom'] = '0px';
         video['style']['right'] = '0px';
